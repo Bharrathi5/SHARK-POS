916787304043eb8d2d3116e6eef00f8765f58e36
Bharrathi5 <bharathimanogaran5@gmail.com>
2024-12-13 00:04:19 +0530

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
