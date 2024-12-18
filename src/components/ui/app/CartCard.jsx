7c957c3933db6e3c72738d4284a1d450f920b146
Bharrathi5 <bharathimanogaran5@gmail.com>
2024-12-18 17:09:33 +0530

    Cart Component

---
+++ src/components/ui/app/CartCard.jsx
@@ -0,0 +1,98 @@
+import PropTypes from "prop-types";
+import { Button } from "../ui/button";
+import { IndianRupee } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/ui/card";
+import { Separator } from "../ui/separator";
+
+const CartCard = ({
+  cart,
+  onIncrementQuantity,
+  onDecrementQuantity,
+  calculateTotal,
+}) => {
+  return (
+    <div>
+      <Card className="w-[300px] bg-muted">
+        <CardHeader>
+          <CardTitle>Cart</CardTitle>
+          <CardDescription>Items on your cart</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="">
+            {cart.length ? (
+              cart.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col gap-4 justify-between items-center space-y-2 mb-6"
+                >
+                  <div className="flex flex-row gap-5 justify-around">
+                    <div className="flex flex-col">
+                      <strong>{item.productName}</strong>
+                      <div className="flex items-center mt-1">
+                        <IndianRupee className="size-4" />
+                        <span className="ml-1">{item.variant.price}</span>
+                      </div>
+                    </div>
+                    <div className="flex items-center">
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        className="mx-2"
+                        onClick={() => onDecrementQuantity(item.variant._id)}
+                      >
+                        -
+                      </Button>
+                      <span>{item.quantity}</span>
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        className="mx-2"
+                        onClick={() => onIncrementQuantity(item.variant._id)}
+                      >
+                        +
+                      </Button>
+                    </div>
+                  </div>
+                  <Separator className="bg-primary " />
+                </div>
+              ))
+            ) : (
+              <div>Your cart is empty!</div>
+            )}
+          </div>
+          <div className="py-4">
+            <strong>Total: ₹{calculateTotal()}</strong>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button>Checkout</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+CartCard.propTypes = {
+  cart: PropTypes.arrayOf(
+    PropTypes.shape({
+      productName: PropTypes.string.isRequired,
+      variant: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+      }).isRequired,
+      quantity: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  onIncrementQuantity: PropTypes.func.isRequired,
+  onDecrementQuantity: PropTypes.func.isRequired,
+  calculateTotal: PropTypes.func.isRequired,
+};
+
+export default CartCard;
--- src/components/ui/app/PointOfSale.jsx
+++ src/components/ui/app/PointOfSale.jsx
@@ -1,17 +1,7 @@
 import { useSelector } from "react-redux";
 import ProductCard from "./ProductCard";
 import { useState } from "react";
-import { Button } from "../ui/button";
-import { IndianRupee } from "lucide-react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/ui/card";
-import { Separator } from "../ui/separator";
+import CartCard from "./CartCard";
 
 const PointOfSale = () => {
   const { products } = useSelector((store) => store.table);
@@ -81,68 +71,13 @@ const PointOfSale = () => {
             ))
           )}
         </div>
-        <div className="flex mt-10">
-          <Card className="w-[300px] bg-muted">
-            <CardHeader>
-              <CardTitle>Cart</CardTitle>
-              <CardDescription>Items on your cart</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className="">
-                {cart.length ? (
-                  cart.map((item, index) => (
-                    <div
-                      key={index}
-                      className="flex flex-col gap-4 justify-between items-center space-y-2 mb-6"
-                    >
-                      <div className="flex flex-row gap-5 justify-around">
-                        <div className="flex flex-col">
-                          <strong>{item.productName}</strong>
-                          <div className="flex items-center mt-1">
-                            <IndianRupee className="size-4" />
-                            <span className="ml-1">{item.variant.price}</span>
-                          </div>
-                        </div>
-                        <div className="flex items-center">
-                          <Button
-                            variant="outline"
-                            size="icon"
-                            className="mx-2"
-                            onClick={() =>
-                              handleDecrementQuantity(item.variant._id)
-                            }
-                          >
-                            -
-                          </Button>
-                          <span>{item.quantity}</span>
-                          <Button
-                            variant="outline"
-                            size="icon"
-                            className="mx-2"
-                            onClick={() =>
-                              handleIncrementQuantity(item.variant._id)
-                            }
-                          >
-                            +
-                          </Button>
-                        </div>
-                      </div>
-                      <Separator className="bg-primary " />
-                    </div>
-                  ))
-                ) : (
-                  <div>Your cart is empty!</div>
-                )}
-              </div>
-              <div className="py-4">
-                <strong>Total: ₹{calculateTotal()}</strong>
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Checkout</Button>
-            </CardFooter>
-          </Card>
-        </div>
+        <div className="flex mt-10"></div>
+        <CartCard
+          cart={cart}
+          onIncrementQuantity={handleIncrementQuantity}
+          onDecrementQuantity={handleDecrementQuantity}
+          calculateTotal={calculateTotal}
+        />
       </div>
     </div>
   );
